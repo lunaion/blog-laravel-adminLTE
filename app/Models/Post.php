6c611709d4d3e1d83132f01a4bc31e431409a331
Javier Luna <lunaion2200@gmail.com>
2023-01-28 23:41:32 -0500

    Creación de BD, relacones, seeders, factorys, inicialización de proyecto

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Relación uno a muchos inversa (User <- Posts)
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    // Relación uno a muchos inversa (Category <- Posts)
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relación muchos a muchos (Posts <-> Categories)
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // Relación uno a uno polimórfica (Post -> Image)
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
