0cc6bdde26c29bb136f733c9eaa71c2a0dcb5d7f
Javier Luna <lunaion2200@gmail.com>
2023-01-31 22:55:52 -0500

    Filtrado de posts por categorías

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,25 @@ class PostController extends Controller
         $posts = Post::where('status', 2)->latest('id')->paginate(8);
 
         return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post){
+
+        $similares = Post::where('category_id', $post->category_id)
+                            ->where('status', 2)
+                            ->where('id', '!=', $post->id)
+                            ->latest('id')
+                            ->take(4)
+                            ->get();
+
+        return view('posts.show', compact('post', 'similares'));
+    }
+
+    public function category(Category $category){
+        $posts = Post::where('category_id', $category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(4);
+        return view('posts.category', compact('posts', 'category'));
     }
 }
