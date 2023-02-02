b5c99ea1010dc6683903f90a7a4d79728b32aff6
Javier Luna <lunaion2200@gmail.com>
2023-02-01 22:23:00 -0500

    Mostrar posts por etiquetas

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mx-auto max-w-5xl px-2 sm:px-6 lg:px-8 py-8">
         <h1 class="uppercase text-center text-3xl font-bold">
-            Categoría: {{$category->name}}
+            Etiqueta: {{$tag->name}}
         </h1>
 
         @foreach ($posts as $post)
