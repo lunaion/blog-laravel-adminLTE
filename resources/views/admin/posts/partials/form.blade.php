1446dd046aecabf794ad77527c859b22a5c90064
Javier Luna <lunaion2200@gmail.com>
2023-02-05 22:32:59 -0500

    Actulización de un post

---
+++ resources/views/admin/posts/partials/form.blade.php
@@ -0,0 +1,107 @@
+<div class="form-group">
+    {!! Form::label('name', 'Nombre') !!}
+    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Digite el nombre del post']) !!}
+
+    @error('name')
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    {!! Form::label('slug', 'Slug') !!}
+    {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Digite el slug del post', 'readonly']) !!}
+
+    @error('slug')
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    {!! Form::label('category_id', 'Categoría') !!}
+    {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+
+    @error('category_id')
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <p class="font-weight-bold">Etiquetas</p>
+
+    @foreach ($tags as $tag)
+        
+        <label class="mr-2">
+            {!! Form::checkbox('tags[]', $tag->id, null) !!}
+            {{$tag->name}}
+        </label>
+
+    @endforeach
+
+    @error('tags')
+        <br>
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <p class="font-weight-bold">Estado</p>
+
+    <label>
+        {!! Form::radio('status', 1, true) !!}
+        Borrador
+    </label>
+
+    <label>
+        {!! Form::radio('status', 2) !!}
+        Publicado
+    </label>
+
+    @error('status')
+        <br>
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="row mb-3">
+    <div class="col">
+        <div class="image-wrapper">
+            @isset ($post->image)
+                <img id="picture" class="w-42 h-40 object-cover object-center" src="{{ url('storage/' . $post->image->url) }}" alt="">
+            @else
+                <img id="picture" src="{{url('vendor/adminlte/dist/img/default-image.jpg')}}" alt="">
+            @endisset
+        </div>
+    </div>
+    <div class="col">
+        <div class="form-group">
+            {!! Form::label('file', 'Imagen que se mostrará en el post') !!}
+            {!! Form::file('file', ['class' => 'form-control-file', 'accept' => 'image/*']) !!}
+
+            @error('file')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
+
+        </div>
+
+        <p>Se recomienda anexar imágenes de 640*480px para mantener una mejor calidad y dimensiones.</p>
+
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('extract', 'Extracto') !!}
+    {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+
+    @error('extract')
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    {!! Form::label('body', 'Cuerpo del post') !!}
+    {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+
+    @error('body')
+        <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
