d4f9ad665ec3fc803d8146339baa4c3eebe5c3ec
Javier Luna <lunaion2200@gmail.com>
2023-02-11 15:18:39 -0500

    Cambio de logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>HELP</b>DESK',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/logo-admin.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
@@ -86,7 +86,7 @@ return [
     'auth_logo' => [
         'enabled' => false,
         'img' => [
-            'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+            'path' => 'vendor/adminlte/dist/img/logo-admin.png',
             'alt' => 'Auth Logo',
             'class' => '',
             'width' => 50,
@@ -109,7 +109,7 @@ return [
     'preloader' => [
         'enabled' => true,
         'img' => [
-            'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+            'path' => 'vendor/adminlte/dist/img/logo-admin.png',
             'alt' => 'AdminLTE Preloader Image',
             'effect' => 'animation__shake',
             'width' => 60,
@@ -349,7 +349,7 @@ return [
             'icon'    => 'fas fa-fw fa-users-cog',
             'submenu' => [
                 [
-                    'text'        => 'Usuarios',
+                    'text'        => 'Lista de usuarios',
                     'route'         => 'admin.users.index',
                     'icon'        => 'fas fa-fw fa-users',
                     'can'        => 'admin.users.index',
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -33,8 +33,8 @@
 
             {{-- Logotipo --}}
             <a href="/" class="flex flex-shrink-0 items-center">
-                <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-                <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                <img class="block h-8 w-auto lg:hidden" src="{{url('vendor/adminlte/dist/img/bhp.png')}}" alt="Your Company">
+                <img class="hidden h-8 w-auto lg:block" src="{{url('vendor/adminlte/dist/img/bhp.png')}}" alt="Your Company">
             </a>
 
             {{-- Menu lg --}}
@@ -56,13 +56,13 @@
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
               {{-- Botón de notificaciones --}}
-              <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              {{-- <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span class="sr-only">View notifications</span>
                   <!-- Heroicon name: outline/bell -->
                   <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                   </svg>
-              </button>
+              </button> --}}
       
               <!-- Profile dropdown -->
               <div class="relative ml-3" x-data="{ open: false }">
